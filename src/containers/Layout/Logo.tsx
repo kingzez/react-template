659d150dd420d19e18e55b6514e3530946d02f48
kingzez <kingzez@sina.com>
2020-04-08 16:12:58 +0800

    ✨ Feat: extra logo components

---
+++ src/containers/Layout/Logo.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import LogoLight from 'assets/wave-light.png'
+
+const Logo = () => (
+  <div className="sider-menu-logo">
+    <img className="" src={LogoLight} alt="logo" />
+    <h1>React Admin</h1>
+  </div>
+)
+
+export default Logo
