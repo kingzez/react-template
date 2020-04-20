78e6dd6e0594eb5b637f5ac52583ba0a66b416c6
kingzez <kingzez@sina.com>
2020-04-20 18:49:54 +0800

    🎨 Style: design login page

--- src/containers/Login/index.tsx
+++ src/containers/Login/index.tsx
@@ -1,22 +1,9 @@
 import React from 'react'
 import { useDispatch } from 'react-redux'
 import { Form, Input, Button } from 'antd'
+import { UserOutlined, LockOutlined } from '@ant-design/icons'
 import * as userAction from 'actions/user'
-
-const layout = {
-  // labelCol: {
-  //   span: 8,
-  // },
-  // wrapperCol: {
-  //   span: 16,
-  // },
-}
-const tailLayout = {
-  // wrapperCol: {
-  //   offset: 8,
-  //   span: 16,
-  // },
-}
+import style from './Login.module.scss'
 
 const Login = () => {
   const dispatch = useDispatch()
@@ -31,44 +18,44 @@ const Login = () => {
   }
 
   return (
-    <Form
-      {...layout}
-      name="basic"
-      initialValues={{
-        remember: true,
-      }}
-      onFinish={onFinish}
-      onFinishFailed={onFinishFailed}>
-      <Form.Item
-        label="用户名"
-        name="username"
-        rules={[
-          {
-            required: true,
-            message: '请输入用户名!',
-          },
-        ]}>
-        <Input />
-      </Form.Item>
+    <div className={style.login}>
+      <div className={style.card}>
+        <div className={style.header}>
+          <span>欢迎登录</span>
+        </div>
+        <div className={style.body}>
+          <Form
+            name="basic"
+            initialValues={{
+              remember: true,
+            }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}>
+            <Form.Item
+              name="username"
+              rules={[{ required: true, message: '请输入用户名!' }]}>
+              <Input prefix={<UserOutlined />} placeholder="用户名" />
+            </Form.Item>
 
-      <Form.Item
-        label="密码"
-        name="password"
-        rules={[
-          {
-            required: true,
-            message: '请输入密码!',
-          },
-        ]}>
-        <Input.Password />
-      </Form.Item>
+            <Form.Item
+              name="password"
+              rules={[{ required: true, message: '请输入密码!' }]}>
+              <Input.Password
+                prefix={<LockOutlined />}
+                type="password"
+                placeholder="密码"
+              />
+            </Form.Item>
 
-      <Form.Item {...tailLayout}>
-        <Button type="primary" htmlType="submit">
-          登录
-        </Button>
-      </Form.Item>
-    </Form>
+            <Form.Item>
+              <Button type="primary" block htmlType="submit">
+                登录
+              </Button>
+            </Form.Item>
+          </Form>
+        </div>
+      </div>
+    </div>
   )
 }
 
